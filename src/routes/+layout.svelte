e7dc2030010d64bfbc398dc1a1b7ed16deee14ff
Ben <ben.a.burgos@gmail.com>
2025-02-09 17:24:00 -0700

    adds protected route logic

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
 <script>
+	import { auth } from '$lib/stores/auth';
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
 	import '../app.css';
+
+	let isAuthenticated = false;
+
+	$: {
+		auth.subscribe(({ isAuthenticated: authStatus }) => {
+			isAuthenticated = authStatus;
+		});
+
+		// Redirect to login if not authenticated
+		if (!isAuthenticated && $page.url.pathname !== '/') {
+			goto('/');
+		}
+	}
 </script>
 
-<slot></slot>
+<slot />
