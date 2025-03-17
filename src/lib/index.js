3d3ffaf0e7b6869a386d64a6b880b017d2c9f94a
Ben <ben.a.burgos@gmail.com>
2025-03-16 17:03:19 -0700

    adds exports to index

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,11 @@
-// place files you want to import through the `$lib` alias in this folder.
+// This file is used to export all the necessary functions and variables from the localStorage.js and auth.js files.
+export {
+	getAllRecords,
+	getRecordById,
+	createRecord,
+	updateRecord,
+	deleteRecord,
+	initializeLocalStorage
+} from '$lib/localStorage';
+export { auth, loginUser, logoutUser } from '$lib/stores/auth';
+export { seedData } from '$lib/data/seedData';
