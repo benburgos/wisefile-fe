8d90aaa33efdb074a4e19ff48009413f78465f2d
Ben <ben.a.burgos@gmail.com>
2025-04-06 12:34:21 -0700

    adds a centralized case status list

---
+++ src/lib/data/caseStatuses.js
@@ -0,0 +1,14 @@
+export const caseStatuses = {
+	'Demand Posted': [],
+	'Demand Expired': [],
+	'Eviction Service': [],
+	'Answer Filed': [],
+	'Court Date': [],
+	Consent: [],
+	Writ: ['Date Requested', 'Date Available', 'Date Received'],
+	Eviction: ['To Be Scheduled', 'Date Scheduled', 'Completed'],
+	Dismissed: ['Filed in Error', 'Paid'],
+	Appeal: [],
+	Bankruptcy: [],
+	'Skipped/Vacated': []
+};
